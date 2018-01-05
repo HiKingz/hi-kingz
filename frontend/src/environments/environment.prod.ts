dc856aa07b7961baba607615a53173cb83577669
Jonas Depoix <jdepoix@seibert-media.net>
2018-01-05 13:48:25 +0100

    prod env vars added

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA8iOUUnJ_Nd7l2_DI4wCR2pUxpH0l3RP8",
+    authDomain: "hikingz-185410.firebaseapp.com",
+    databaseURL: "https://hikingz-185410.firebaseio.com",
+    projectId: "hikingz-185410",
+    storageBucket: "hikingz-185410.appspot.com",
+    messagingSenderId: "1069159165360"
+  },
+  mapbox: {
+    accessToken: 'pk.eyJ1Ijoic3RhbmRieW1vZGUiLCJhIjoiY2o5NzZqMTdmMDQzMDJ3cnc5aW5ueXNmeSJ9.q_l4vASYPsSkfHrAxPGjbw'
+  }
 };
