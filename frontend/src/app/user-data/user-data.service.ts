9f03130b9afded6bf0bf9a7bc1ed67fdb5432959
Jonas Depoix <jdepoix@seibert-media.net>
2018-01-18 15:20:44 +0100

    observables added to UserData service to be able to track when UserData is loaded

--- frontend/src/app/user-data/user-data.service.ts
+++ frontend/src/app/user-data/user-data.service.ts
@@ -8,22 +8,22 @@ import {AuthenticationService} from '../authentication/authentication.service';
 import 'rxjs/add/operator/first';
 import * as firebase from 'firebase';
 import {Subscription} from 'rxjs/Subscription';
+import {Subject} from 'rxjs/Subject';
 
 @Injectable()
 export class UserDataService extends FirestoreDataService<UserData> {
+  private _currentUserDataLoadedSubject: Subject<UserData> = new Subject<UserData>();
+  private _currentUserDataUnsetSubject: Subject<void> = new Subject<void>();
   private _currentUserDataSubscription: Subscription;
   protected readonly _collectionPath: string = 'user-data';
   public currentUserData: UserData = null;
+  public onCurrentUserDataLoaded: Observable<UserData> = this._currentUserDataLoadedSubject.asObservable();
+  public onCurrentUserDataUnset: Observable<void> = this._currentUserDataUnsetSubject.asObservable();
 
   constructor(db: AngularFirestore, private _authService: AuthenticationService) {
     super(db);
     this._authService.onLogin.subscribe(() => this._loadCurrentUserData());
-    this._authService.onLogout.subscribe(() => {
-      if (this._currentUserDataSubscription && !this._currentUserDataSubscription.closed) {
-        this._currentUserDataSubscription.unsubscribe();
-      }
-      this.currentUserData = null;
-    });
+    this._authService.onLogout.subscribe(() => this._unsetCurrentUserData);
   }
 
   public async create(user: UserSignature): Promise<Observable<FirebaseItem<UserData>>> {
@@ -52,11 +52,20 @@ export class UserDataService extends FirestoreDataService<UserData> {
           this._currentUserDataSubscription = this.getById(userId).subscribe(
             (userDataItem) => {
               this.currentUserData = userDataItem.item;
+              this._currentUserDataLoadedSubject.next(this.currentUserData);
             }
           );
         }
       }
     );
+  }
+
+  private _unsetCurrentUserData(): void {
+    if (this._currentUserDataSubscription && !this._currentUserDataSubscription.closed) {
+      this._currentUserDataSubscription.unsubscribe();
+    }
+    this.currentUserData = null;
+    this._currentUserDataUnsetSubject.next();
   }
 
   public update(user: FirebaseItem<UserData>): Promise<void> {
